f519ff5d4aa96b226e84d6d45599eab382db6285
sebastienQNLT <45787958+sebastienQNLT@users.noreply.github.com>
2018-12-16 15:59:23 +0100

    Create packages.R

---
+++ packages/packages.R
@@ -0,0 +1,6 @@
+install.packages("dplyr")
+install.packages("fastDummies")
+install.packages("DataExplorer")
+install.packages("ggplot2")
+install.packages("FactoMineR")
+install.packages("factoextra")
